bb47499eef484550fd5e80f5abd1c0843d1320e2
José Luiz Gonçalves <joseluizgo69@gmail.com>
2026-02-18 17:43:35 -0300

    Condiçoes

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`voce passou do limite de velocidade, multado!`)
+}
+console.log('Dirija sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil') {
+    console.log(' vc é Brasileiro')
+} else {
+    console.log(' vc é Estrangeiro')
+}
