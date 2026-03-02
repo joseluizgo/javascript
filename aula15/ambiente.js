4c394a852d2cf75e7da54e059975a6b9c0a6340f
José Luiz Gonçalves <joseluizgo69@gmail.com>
2026-03-02 14:07:12 -0300

    finalizando os estudos em js

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5 , 8, 2, 9, 3]
+var n1 = 2
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[n1]}`)
+let pos = num.indexOf(8)
+console.log(`O valor 8 esta na posição ${pos}`)
